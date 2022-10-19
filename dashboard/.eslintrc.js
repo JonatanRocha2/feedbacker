c51859d7880778345ba5df9db288b167a771c893
Jonatan Rocha <jonatanivanhoe@gmai.com>
2022-10-19 19:50:58 -0300

    configuracao das rotas

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    'vue/multi-word-component-names': 0,
+    "eol-last": 0,
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
