1159e4d39cb210d58cd2edd1e8b412c5135e8aeb
Jonatan Rocha <jonatanivanhoe@gmai.com>
2022-10-20 18:16:07 -0300

    primeira parte da tela inicial

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <h1 class="text-brand-info">Aqui é a Home!</h1>
+  <custom-header
+    @create-account="handlerCreateAccount"
+    @login="handlerLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-madium text-center text-gray-800">feedbacker © 2022</p>
+  </div>
 </template>
 
 <script>
-export default {
 
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+import { useRoute } from 'vue-router'
+import { onMounted } from '@vue/runtime-core'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRoute()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({
+          name: 'Feedbacks'
+        })
+      }
+    })
+    function handlerLogin () {
+    }
+    function handlerAccountCreate () {
+    }
+
+    return {
+      handlerLogin,
+      handlerAccountCreate
+    }
+  }
 }
 </script>
